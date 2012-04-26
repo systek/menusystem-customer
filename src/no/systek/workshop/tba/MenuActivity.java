b0db73b4ec7720d97426d95b9bfc334e5ebfaea9
Oyvind Bo Syrstad <oyvind.bo.syrstad@gmail.com>
2012-04-26 23:09:46 +0200

    Added Android skeleton

---
+++ src/no/systek/workshop/tba/MenuActivity.java
@@ -0,0 +1,15 @@
+package no.systek.workshop.tba;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MenuActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
